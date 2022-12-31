24f2db7c6cdef7fc0b7ac1c2c48de17e66f6595b
Dolev Hadar <dolevc2@gmail.com>
2022-12-31 19:09:01 +0200

    feat(nvim): replace Comment.nvim with mini.comment
    Signed-off-by: Dolev Hadar <dolevc2@gmail.com>

---
+++ .config/nvim/lua/dlvhdr/plugins/mini.lua
@@ -0,0 +1,22 @@
+local mini = {
+  "echasnovski/mini.nvim",
+  event = "VeryLazy",
+}
+
+local specs = { mini, "JoosepAlviste/nvim-ts-context-commentstring" }
+
+function mini.comment()
+  require("mini.comment").setup({
+    hooks = {
+      pre = function()
+        require("ts_context_commentstring.internal").update_commentstring({})
+      end,
+    },
+  })
+end
+
+function mini.config()
+  mini.comment()
+end
+
+return specs
