6301b013d086fd323ca3fa3c90f4b85d49106697
Dolev Hadar <dolevc2@gmail.com>
2023-07-07 22:01:56 +0300

    fix(scripts): commit script to ignore yarn.lock

--- .config/fish/functions/commit.fish
+++ .config/fish/functions/commit.fish
@@ -1,4 +1,4 @@
 function commit
-  set msg (git diff HEAD | mods "create a one line convetional commit message from this git diff")
+  set msg (git diff HEAD -- . !yarn.lock | mods "create a one line convetional commit message from this git diff")
   git commit -m $msg
 end
