315c6360c965e06ed44541598c44e57c3f4ffb94
Dolev Hadar <dolevc2@gmail.com>
2023-08-26 19:47:10 +0300

    fix: change gup to go-global-update

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,5 +1,5 @@
 function update
-  set cmds "brew update && brew upgrade" "brew upgrade --cask --greedy" "npm update -g --fetch-timeout 3000" "yarn global upgrade" "gh extensions upgrade --all" "gup update" "rustup update" "tpmu # update tmux plugins"
+  set cmds "brew update && brew upgrade" "brew upgrade --cask --greedy" "npm update -g --fetch-timeout 3000" "yarn global upgrade" "gh extensions upgrade --all" "go-global-update" "rustup update" "tpmu # update tmux plugins"
   set chose (gum choose --no-limit $cmds)
   for cmd in $chose
     eval "$cmd"
