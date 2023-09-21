4b8d79cb0ca4c264f8e69c7c91fddb62ac23baf3
Dolev Hadar <dolevc2@gmail.com>
2023-09-21 23:52:06 +0300

    fix(kubectl): kgp script to not enter newline

--- .config/fish/functions/kgp.fish
+++ .config/fish/functions/kgp.fish
@@ -1,5 +1,5 @@
 function kgp
-  set pod (kubectl get pod --no-headers -o json | jq -rj '.items[0].metadata.name')
-  echo $pod | pbcopy
-  echo "Copied pod name $pod to clipboard"
+  set pod (kubectl get pod --no-headers -o json | jq -j '.items[0].metadata.name')
+  echo -n $pod | pbcopy
+  gum format --theme=dark "✅ Copied pod name `$pod` to clipboard"
 end
