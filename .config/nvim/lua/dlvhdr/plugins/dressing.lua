e79497c57da0db26a4ab8c77ea2cdd16a2f85af6
Dolev Hadar <dolevc2@gmail.com>
2023-10-09 19:05:09 +0300

    fix(nvim): dressing

--- .config/nvim/lua/dlvhdr/plugins/dressing.lua
+++ .config/nvim/lua/dlvhdr/plugins/dressing.lua
@@ -1,16 +1,13 @@
 return {
   "stevearc/dressing.nvim",
   lazy = true,
-  init = function()
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.select = function(...)
-      require("lazy").load({ plugins = { "dressing.nvim" } })
-      return vim.ui.select(...)
-    end
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.input = function(...)
-      require("lazy").load({ plugins = { "dressing.nvim" } })
-      return vim.ui.input(...)
-    end
+  config = function()
+    require("dressing").setup({
+      input = {
+        win_options = {
+          winhighlight = "NormalFloat:DiagnosticError",
+        },
+      },
+    })
   end,
 }
