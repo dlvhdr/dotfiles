bb3cde3b0d2df4f525acb9493de137d56378c8e0
Dolev Hadar <dolevc2@gmail.com>
2023-10-26 17:51:34 +0300

    feat(nvim): misc

---
+++ .config/nvim/lua/dlvhdr/plugins/yaml-companion.lua
@@ -0,0 +1,22 @@
+return {
+  "someone-stole-my-name/yaml-companion.nvim",
+  requires = {
+    { "neovim/nvim-lspconfig" },
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+  },
+  ft = { "yaml", "helm" },
+  config = function()
+    require("telescope").load_extension("yaml_schema")
+
+    local cfg = require("yaml-companion").setup({
+      schemas = {
+        {
+          name = "Kubernetes 1.22.4",
+          uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json",
+        },
+      },
+    })
+    require("lspconfig")["yamlls"].setup(cfg)
+  end,
+}
--- .config/nvim/lua/dlvhdr/snippets/init.lua
+++ .config/nvim/lua/dlvhdr/snippets/init.lua
@@ -4,5 +4,6 @@ M.go = require("dlvhdr.snippets.go")
 M.lua = require("dlvhdr.snippets.lua")
 M.typescript = require("dlvhdr.snippets.typescript")
 M.typescriptreact = require("dlvhdr.snippets.typescriptreact")
+M.yaml = require("dlvhdr.snippets.yaml")
 
 return M
