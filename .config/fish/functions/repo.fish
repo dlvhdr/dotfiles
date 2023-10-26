c45b19acafcf1fe3deb084566f9c6f7c53ec4bc1
Dolev Hadar <dolevc2@gmail.com>
2023-10-26 17:49:34 +0300

    fix(fish): repo and root scripts

--- .config/fish/functions/repo.fish
+++ .config/fish/functions/repo.fish
@@ -1,5 +1,5 @@
 function repo
-  set repos (find ~/code/wix ~/code/personal ~/code/playground -type d  -mindepth 1 -maxdepth 1 | sed "s#$HOME/code/##" | grep -v "DS_Store")
+  set repos (find ~/code/komodor ~/code/personal ~/code/playground -type d  -mindepth 1 -maxdepth 1 | sed "s#$HOME/code/##" | grep -v "DS_Store")
 
   set selected (echo $repos | string split -n " " | gum filter --height 10 --indicator="→")
   
--- .config/fish/functions/root.fish
+++ .config/fish/functions/root.fish
@@ -1,3 +1,3 @@
 function root
-  cd (git root)
+  cd (git root 2> /dev/null) || cd "$HOME"
 end
