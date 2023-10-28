10efb6b8e9fca3642637d67d5e680657a5afef15
Dolev Hadar <dolevc2@gmail.com>
2023-10-28 15:33:51 +0300

    feat(nvim): remove leap and use mini

--- .config/nvim/lua/dlvhdr/plugins/mini.lua
+++ .config/nvim/lua/dlvhdr/plugins/mini.lua
@@ -44,19 +44,7 @@ function mini.move()
 end
 
 function mini.surround()
-  require("mini.surround").setup({
-    mappings = {
-      add = "gza", -- Add surrounding in Normal and Visual modes
-      delete = "gzd", -- Delete surrounding
-      find = "gzf", -- Find surrounding (to the right)
-      find_left = "gzF", -- Find surrounding (to the left)
-      highlight = "gzh", -- Highlight surrounding
-      replace = "gzr", -- Replace surrounding
-      update_n_lines = "gzn", -- Update `n_lines`
-      suffix_last = "l", -- Suffix to search with "prev" method
-      suffix_next = "n", -- Suffix to search with "next" method
-    },
-  })
+  require("mini.surround").setup({})
 end
 
 function mini.config()
