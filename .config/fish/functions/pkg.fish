0678b1540b391fcc0f508ad2ef00244d7df09a45
Dolev Hadar <dolevc2@gmail.com>
2023-11-17 21:21:38 +0200

    feat(fish): add komodor

--- .config/fish/functions/pkg.fish
+++ .config/fish/functions/pkg.fish
@@ -1,16 +1,22 @@
 function pkg
   set head (git rev-parse --show-toplevel)
-  set packages (ls -D -r -s accessed --no-icons "$head"/packages 2>/dev/null) 
+  set packages (ls -1 -D -r -s accessed --no-icons "$head"/packages "$head"/services "$head"/shared "$head"/tests 2>/dev/null | grep '^[a-zA-Z]') 
 
   if not set -q packages[1]
-    echo "no packages"
+    echo "no packages/services"
     return
   end
 
   set selected (echo $packages  | string split -n " " | gum filter --indicator="→" --height=20)
   
   if test -n "$selected"
-    cd "$head/packages/$selected" || return
+    if test -d "$head/packages/$selected"
+      cd "$head/packages/$selected"
+    else if test -d "$head/services/$selected"
+      cd "$head/services/$selected"
+    else if test -d "$head/shared/$selected"
+      cd "$head/shared/$selected"
+    end
   end
 end
 
