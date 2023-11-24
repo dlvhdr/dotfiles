4701eb59cc75a56f363903653e25176d5041bd77
Dolev Hadar <dolevc2@gmail.com>
2023-11-24 20:24:12 +0200

    feat(nvim): ts remove unused imports + auto import

--- .config/nvim/lua/dlvhdr/plugins/lsp/servers/tsserver.lua
+++ .config/nvim/lua/dlvhdr/plugins/lsp/servers/tsserver.lua
@@ -8,6 +8,26 @@ M.setup = function(opts)
     capabilities = opts.capabilities,
     root_dir = lspconfig.util.root_pattern("package.json"),
   })
+
+  vim.keymap.set("n", "<leader>cr", function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.removeUnused.ts" },
+        diagnostics = {},
+      },
+    })
+  end, { desc = "Remove Unused Imports" })
+
+  vim.keymap.set("n", "<leader>ca", function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.addMissingImports" },
+        diagnostics = {},
+      },
+    })
+  end, { desc = "Add Missing Imports" })
 end
 
 return M
