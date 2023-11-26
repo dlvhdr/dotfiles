adcfa81f52ad80de794a2fb8d1df411c49064c0f
Dolev Hadar <dolevc2@gmail.com>
2023-11-26 10:24:34 +0200

    feat(nvim): use gitlinker for more things

--- .config/nvim/lua/dlvhdr/plugins/gitlinker.lua
+++ .config/nvim/lua/dlvhdr/plugins/gitlinker.lua
@@ -2,17 +2,39 @@ return {
   "linrongbin16/gitlinker.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
-    require("gitlinker").setup({})
+    require("gitlinker").setup({
+      router = {
+        main_branch = {
+          ["^github%.com"] = "https://github.com/"
+            .. "{_A.USER}/"
+            .. "{_A.REPO}/blob/main/" -- always 'main' branch
+            .. "{_A.FILE}"
+            .. "?&lines={_A.LSTART}"
+            .. "{_A.LEND > _A.LSTART and ('&lines-count=' .. _A.LEND - _A.LSTART + 1) or ''}",
+        },
+        master_branch = {
+          ["^github%.com"] = "https://github.com/"
+            .. "{_A.USER}/"
+            .. "{_A.REPO}/blob/master/" -- always 'master' branch
+            .. "{_A.FILE}"
+            .. "?&lines={_A.LSTART}"
+            .. "{_A.LEND > _A.LSTART and ('&lines-count=' .. _A.LEND - _A.LSTART + 1) or ''}",
+        },
+      },
+    })
   end,
   keys = {
     {
       "<leader>gym",
-      function()
-        local url = vim.fn.system("gh browse -n " .. vim.fn.expand("%") .. ":" .. vim.api.nvim_win_get_cursor(0)[1])
-        vim.api.nvim_command("let @+ = '" .. url .. "'")
-        vim.notify(url)
-      end,
+      "<cmd>GitLink main_branch<cr>",
       desc = "Copy line URL (main)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>gyM",
+      "<cmd>GitLink master_branch<cr>",
+      desc = "Copy line URL (master)",
+      mode = { "n", "v" },
     },
     {
       "<leader>gyc",
