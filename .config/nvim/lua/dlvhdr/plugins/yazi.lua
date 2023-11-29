700b3a47f13ecfe1c6c08eed47cc12a992c651bb
Dolev Hadar <dolevc2@gmail.com>
2023-11-29 20:04:40 +0200

    feat(nvim): yazi over lf

---
+++ .config/nvim/lua/dlvhdr/plugins/yazi.lua
@@ -0,0 +1,11 @@
+return {
+  "DreamMaoMao/yazi.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  cmd = "Yazi",
+  keys = {
+    { "<leader>fe", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+  },
+}
