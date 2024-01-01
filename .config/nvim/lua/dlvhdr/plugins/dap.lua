327b22980bb193f282563e99258aca4f98eb4420
Dolev Hadar <dolevc2@gmail.com>
2024-01-01 23:38:09 +0200

    feat(nvim): attempt at node debugging

--- .config/nvim/lua/dlvhdr/plugins/dap.lua
+++ .config/nvim/lua/dlvhdr/plugins/dap.lua
@@ -35,6 +35,49 @@ return {
         dap.listeners.before.event_exited["dapui_config"] = function()
           dapui.close({})
         end
+
+        vim.print("Configuring dap-vscode-js")
+        vim.print(
+          require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+            .. "/js-debug/src/dapDebugServer.js",
+          "${port}"
+        )
+        if not dap.adapters["pwa-node"] then
+          require("dap").adapters["pwa-node"] = {
+            type = "server",
+            host = "localhost",
+            port = "${port}",
+            executable = {
+              command = "node",
+              -- 💀 Make sure to update this path to point to your installation
+              args = {
+                require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+                  .. "/js-debug/src/dapDebugServer.js",
+                "${port}",
+              },
+            },
+          }
+        end
+        for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+          if not dap.configurations[language] then
+            dap.configurations[language] = {
+              {
+                type = "pwa-node",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+              },
+              {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require("dap.utils").pick_process,
+                cwd = "${workspaceFolder}",
+              },
+            }
+          end
+        end
       end,
     },
     -- virtual text for the debugger
