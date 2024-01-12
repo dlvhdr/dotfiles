fb500db703b52e57f6ac874ae712c79b6384ede5
Dolev Hadar <dolevc2@gmail.com>
2024-01-12 21:30:03 +0200

    feat(nvim): minimalistic fidget

--- .config/nvim/lua/dlvhdr/plugins/fidget.lua
+++ .config/nvim/lua/dlvhdr/plugins/fidget.lua
@@ -1,20 +1,45 @@
 return {
   "j-hui/fidget.nvim",
   event = "LspAttach",
-  enabled = false,
   config = function()
-    vim.cmd("hi! FidgetTitle ctermbg=NONE guibg=NONE")
-    vim.cmd("hi! FidgetTask ctermbg=NONE guibg=NONE")
+    vim.api.nvim_set_hl(0, "FidgetTitle", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "FidgetTask", { bg = "NONE" })
     require("fidget").setup({
       progress = {
-        ignore = { "lua_ls" },
         display = {
+          done_icon = "󰄬",
           done_ttl = 1,
+          format_message = function(msg)
+            return ""
+          end,
+          format_annote = function(msg)
+            return ""
+          end,
+          format_group_name = function(msg)
+            return ""
+          end,
         },
+        notification_group = function()
+          return "some_lsp"
+        end,
       },
       notification = {
         window = {
+          align = "top",
           winblend = 0,
+          x_padding = "0",
+          y_padding = "0",
+        },
+        view = {
+          group_separator = "",
+          render_message = function(msg, cnt)
+            return msg
+          end,
+        },
+      },
+      integration = {
+        ["nvim-tree"] = {
+          enable = true,
         },
       },
     })
