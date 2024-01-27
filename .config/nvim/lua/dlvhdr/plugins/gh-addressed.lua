5f41ae2eccec24855c0acc05876fe58f91c2aaa2
Dolev Hadar <dolevc2@gmail.com>
2024-01-27 15:11:12 +0200

    feat(nvim): wip gh-addressed plugin

---
+++ .config/nvim/lua/dlvhdr/plugins/gh-addressed.lua
@@ -0,0 +1,9 @@
+return {
+  "dlvhdr/gh-addressed.nvim",
+  dir = "/Users/dlvhdr/code/personal/gh-addressed.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+  cmd = "GhReviewComments",
+  keys = {
+    { "<leader>gc", "<cmd>GhReviewComments<cr>", desc = "GitHub Review Comments" },
+  },
+}
