063147e272b5e111ea9f0ac4f6c1d549d6949d1a
Dolev Hadar <dolevc2@gmail.com>
2024-02-16 21:56:11 +0200

    feat(tmux): open-dotfiles.fish to open existing nvim if exist

--- .config/tmux/open-dotfiles.fish
+++ .config/tmux/open-dotfiles.fish
@@ -4,6 +4,19 @@ if not tmux has-session -t dotfiles 2>/dev/null;
   tmux new-session -d -s dotfiles -c '$HOME/dotfiles'
 end
 
-tmux switch-client -t dotfiles;
+set windows (tmux list-windows -t dotfiles -F '#{pane_tty} #{window_index}')
+for window in $windows
+  set -l vars (string split " " $window)
+  set tty $vars[1]
+  set index $vars[2]
+
+  set window_name (/Users/dlvhdr/.config/tmux/tty2procname $tty)
+  if string match -q "nvim" $window_name
+    tmux switch-client -t "dotfiles:$index"
+    return
+  end
+end
+
+tmux switch-client -t dotfiles
 tmux neww
 tmux send-keys -t dotfiles 'cd $HOME/dotfiles && ./.t' Enter;
