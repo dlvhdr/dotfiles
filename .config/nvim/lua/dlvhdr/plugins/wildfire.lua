7fbc71d2a4a897dc94396f691f57ebdf6dc5e4bb
Dolev Hadar <dolevc2@gmail.com>
2024-03-01 18:31:04 +0200

    feat(nvim): wildfire.nvim for better incremental selection

---
+++ .config/nvim/lua/dlvhdr/plugins/wildfire.lua
@@ -0,0 +1,8 @@
+return {
+  "sustech-data/wildfire.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("wildfire").setup()
+  end,
+}
