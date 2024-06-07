e60f5c3a9895329edf7249fe286cd7224d2a92a7
Dolev Hadar <dolevc2@gmail.com>
2024-06-07 16:59:32 +0300

    fix(nvim): remove { as surrounding char with visual-surround.nvim

--- .config/nvim/lua/dlvhdr/plugins/visual-surround.lua
+++ .config/nvim/lua/dlvhdr/plugins/visual-surround.lua
@@ -2,6 +2,8 @@ return {
   "NStefan002/visual-surround.nvim",
   event = "BufReadPost",
   config = function()
-    require("visual-surround").setup({})
+    require("visual-surround").setup({
+      surround_chars = { "[", "]", "(", ")", "'", '"', "`" },
+    })
   end,
 }
