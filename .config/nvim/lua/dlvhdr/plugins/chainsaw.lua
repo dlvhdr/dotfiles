0a0f644eb443074b3aac545f53bc6bc489c08f6d
Dolev Hadar <dolevc2@gmail.com>
2024-06-18 01:33:51 +0300

    fix(nvim): chainsaw disable prettier

--- .config/nvim/lua/dlvhdr/plugins/chainsaw.lua
+++ .config/nvim/lua/dlvhdr/plugins/chainsaw.lua
@@ -1,14 +1,46 @@
 return {
   "chrisgrieser/nvim-chainsaw",
   config = function()
+    local beep_format = {
+      "/* prettier-ignore */ // %s",
+      'console.log("%s beep %s");',
+    }
+    local var_format = {
+      "/* prettier-ignore */ // %s",
+      'console.log("%s %s:", %s);',
+    }
+    local message_format = {
+      "/* prettier-ignore */ // %s",
+      'console.log("%s ");',
+    }
     ---@diagnostic disable: missing-fields
     require("chainsaw").setup({
-      marker = "[🪲 dlvhdr]",
+      marker = "[🪲 dolev]",
+      logStatements = {
+        beepLog = {
+          javascript = beep_format,
+          javascriptreact = beep_format,
+          typescript = beep_format,
+          typescriptreact = beep_format,
+        },
+        variableLog = {
+          javascript = var_format,
+          javascriptreact = var_format,
+          typescript = var_format,
+          typescriptreact = var_format,
+        },
+        messageLog = {
+          javascript = message_format,
+          javascriptreact = message_format,
+          typescript = message_format,
+          typescriptreact = message_format,
+        },
+      },
     })
   end,
   keys = {
     {
-      "<leader>dll",
+      "<leader>dlm",
       function()
         require("chainsaw").messageLog()
       end,
