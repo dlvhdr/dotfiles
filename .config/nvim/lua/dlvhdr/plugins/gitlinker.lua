04d7a9f3d18f0e66b32fc88203e3a81729db0e38
Dolev Hadar <dolevc2@gmail.com>
2024-06-21 16:39:53 +0300

    fix(nvim): gitlinker

--- .config/nvim/lua/dlvhdr/plugins/gitlinker.lua
+++ .config/nvim/lua/dlvhdr/plugins/gitlinker.lua
@@ -2,37 +2,7 @@ return {
   "linrongbin16/gitlinker.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
-    require("gitlinker").setup({
-      --   router = {
-      --     default_branch = {
-      --       ["^github%.com"] = "https://github.com/"
-      --         .. "{_A.USER}/"
-      --         .. "{_A.REPO}/blob/"
-      --         .. "{_A.DEFAULT_BRANCH}/" -- always 'master'/'main' branch
-      --         .. "{_A.FILE}?plain=1" -- '?plain=1'
-      --         .. "#L{_A.LSTART}"
-      --         .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-      --     },
-      --     current_branch = {
-      --       ["^github%.com"] = "https://github.com/"
-      --         .. "{_A.USER}/"
-      --         .. "{_A.REPO}/blob/"
-      --         .. "{_A.CURRENT_BRANCH}/" -- always current branch
-      --         .. "{_A.FILE}?plain=1" -- '?plain=1'
-      --         .. "#L{_A.LSTART}"
-      --         .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-      --     },
-      --     blame_default_branch = {
-      --       ["^github%.com"] = "https://github.com/"
-      --         .. "{_A.USER}/"
-      --         .. "{_A.REPO}/blame/"
-      --         .. "{_A.DEFAULT_BRANCH}/"
-      --         .. "{_A.FILE}?plain=1" -- '?plain=1'
-      --         .. "#L{_A.LSTART}"
-      --         .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-      --     },
-      --   },
-    })
+    require("gitlinker").setup({})
   end,
   keys = {
     {
