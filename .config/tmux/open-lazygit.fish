5b5839133bd892610da74acd2f216d7a979aad79
Dolev Hadar <dolevc2@gmail.com>
2024-08-17 23:26:02 +0300

    feat(tmux): open gh-dash and lazygit side by side

--- .config/tmux/open-lazygit.fish
+++ .config/tmux/open-lazygit.fish
@@ -1,25 +1,23 @@
 #!/opt/homebrew/bin/fish
 
-set windows (tmux list-windows -F '#{pane_tty} #{window_index} #{pane_current_command} #{window_name}')
-for window in $windows
-  set -l vars (string split " " $window)
-  set tty $vars[1]
-  set index $vars[2]
-  set pane_command $vars[3]
-set window_name $vars[4]
-
-  # set window_name (/Users/dlvhdr/.config/tmux/tty2procname $tty)
-  if string match -q "lazygit" $pane_command
-    tmux switch-client -t ":$index"
-    return
-  end
-
-  if string match -q "lazygit" $window_name
-    tmux switch-client -t ":$window_name"
-    tmux send-keys -t ":lazygit" "lazygit" Enter;
-    return
-  end
+WINDOWS = (tmux list-windows -F "#{window_name}")
+if contains "gh-dash" $WINDOWS
+    tmux switch-client -t :gh-dash
+    exit 0
 end
 
-tmux neww -n "lazygit"
-tmux send-keys -t ":lazygit" "lazygit" Enter;
+set SESS_PATH (tmux display-message -p -F "#{session_path}")
+tmux new-window -S -n gh-dash "FF_REPO_VIEW= gh dash $SESS_PATH" 
+tmux switch-client -t :gh-dash
+
+tmux set-option -p -t 1 pane-border-format '#{?pane_active,#[fg=green],#[fg=#63697F]}gh-dash#{?window_zoomed_flag, #[fg=white]#{pane_index}/#{window_panes} ,} '
+
+set NUM_PANES (tmux display-message -p -F "#{window_panes}")
+if test $NUM_PANES -eq 2
+    exit 0
+end
+
+tmux splitw -h -v "lazygit -p $SESS_PATH"
+ pane-border-format "lazygit"
+
+tmux set-option -p -t 2 pane-border-format '#{?pane_active,#[fg=green],#[fg=#63697F]}lazygit#{?window_zoomed_flag, #[fg=white]#{pane_index}/#{window_panes} ,} '
