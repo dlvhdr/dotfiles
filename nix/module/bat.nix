e4f6ae5d02c26e783b73b50f7daac5e78bbdf6b1
Dolev Hadar <dolevc2@gmail.com>
2024-12-20 22:56:26 +0200

    chore: update using update-nix-fetchgit

--- nix/module/bat.nix
+++ nix/module/bat.nix
@@ -12,8 +12,8 @@
           pkgs.fetchFromGitHub {
             owner = "folke";
             repo = "tokyonight.nvim";
-            rev = "9758827c3b380ba89da4a2212b6255d01afbcf08";
-            sha256 = "sha256-qEmfBs+rKP25RlS7VxNSw9w4GnlZiiEchs17nJg7vsE=";
+            rev = "45d22cf0e1b93476d3b6d362d720412b3d34465c";
+            sha256 = "1038ff6i8csxx3cqccgbpv06slvbcs534cfkq7s58ww2vvldm7sc";
           }
           + "/extras/sublime/tokyonight_night.tmTheme";
       };
