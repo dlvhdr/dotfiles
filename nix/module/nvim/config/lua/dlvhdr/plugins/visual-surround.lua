20a6fc08d03fc84d2f8c3b57a8c3ec50b3e21898
Dolev Hadar <dolevc2@gmail.com>
2025-01-17 22:35:43 +0200

    feat(nvim): more keymaps for visual-surround

--- nix/module/nvim/config/lua/dlvhdr/plugins/visual-surround.lua
+++ nix/module/nvim/config/lua/dlvhdr/plugins/visual-surround.lua
@@ -5,5 +5,24 @@ return {
     require("visual-surround").setup({
       surround_chars = { "[", "]", "(", ")", "'", '"', "`" },
     })
+
+    vim.keymap.set("v", "sc", function()
+      require("visual-surround").surround("{")
+    end)
+    vim.keymap.set("v", "sp", function()
+      require("visual-surround").surround("(")
+    end)
+    vim.keymap.set("v", "sb", function()
+      require("visual-surround").surround("[")
+    end)
+    vim.keymap.set("v", "st", function()
+      require("visual-surround").surround("`")
+    end)
+    vim.keymap.set("v", "sq", function()
+      require("visual-surround").surround('"')
+    end)
+    vim.keymap.set("v", "ss", function()
+      require("visual-surround").surround("'")
+    end)
   end,
 }
