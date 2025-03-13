34e0a7b0a8e8ba76b1e289a5b801ac7cf8f5f6d1
Dolev Hadar <dolevc2@gmail.com>
2025-03-13 18:03:02 +0200

    fix(nvim): dap

--- nix/module/nvim/config/lua/dlvhdr/plugins/dap.lua
+++ nix/module/nvim/config/lua/dlvhdr/plugins/dap.lua
@@ -101,6 +101,21 @@ return {
             },
           }
         end
+
+        if not dap.adapters["node"] then
+          dap.adapters["node"] = function(cb, config)
+            if config.type == "node" then
+              config.type = "pwa-node"
+            end
+            local nativeAdapter = dap.adapters["pwa-node"]
+            if type(nativeAdapter) == "function" then
+              nativeAdapter(cb, config)
+            else
+              cb(nativeAdapter)
+            end
+          end
+        end
+
         for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
           if not dap.configurations[language] then
             dap.configurations[language] = {
@@ -179,9 +194,6 @@ return {
       keys = {
         { "<leader>dt", function() require('dap-go').debug_test() end, desc = "Debug Test" },
       },
-    },
-    {
-      "mxsdev/nvim-dap-vscode-js",
     },
     {
       "mfussenegger/nvim-dap-python",
